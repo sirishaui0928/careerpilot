40ec7aa21857d01eebf9f034b9945a3d15d9fa3a
sirishaui0928 <sirishaui0928@gmail.com>
2026-05-29 13:22:57 +0530

    Fix CORS for production

--- server/server.js
+++ server/server.js
@@ -23,7 +23,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      ["http://localhost:5173", "https://careerpilot-lilac-five.vercel.app"],
+
+    methods: ["GET", "POST", "PUT", "DELETE"], credentials: true
+  }));
 
 app.use(express.json());
 
