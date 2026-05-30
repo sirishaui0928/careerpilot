c1bb790d55328b9c0880e893982f0f51095d54d7
sirishaui0928 <sirishaui0928@gmail.com>
2026-05-30 12:02:00 +0530

    Simplify CORS configuration

--- server/server.js
+++ server/server.js
@@ -26,18 +26,7 @@ const allowedOrigins = [
   "https://careerpilot-lilac-five.vercel.app"
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+app.use(cors({ origin: true, credentials: true, }));
 
 
 app.use(express.json());
