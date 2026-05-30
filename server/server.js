ba250aadb79fa63331087f6ab2abf0fb2ebf58c0
sirishaui0928 <sirishaui0928@gmail.com>
2026-05-30 11:54:44 +0530

    Remove invalid app.options wildcard

--- server/server.js
+++ server/server.js
@@ -38,11 +38,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
-app.options("*",
-  cors({
-    origin: ["http://localhost:5173", "https://careerpilot-lilac-five.vercel.app"],
-    credentials: true
-  }));
+
 
 app.use(express.json());
 
